e331548c0646da07937e92ddee125a71737fce20
AI PC <atkola12345@gmail.com>
2025-08-15 17:05:54 +0530

    02 : database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+const Page = async () => {
 
-const Page = () => {
   return (
-    <div className="font-bold text-2xl">
-      Hello World 
-      <Button variant="destructive">
-        Button
-      </Button>
+    <div>
+      Hello World!
     </div>
   )
 }
